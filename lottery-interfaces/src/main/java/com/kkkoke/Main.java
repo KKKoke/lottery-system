4bfff85feec779cd96085add9744bea89d63e871
kkkoke <1120746320@qq.com>
2023-06-28 23:25:15 +0800

    feat: 搭建 DDD 四层架构

---
+++ lottery-interfaces/src/main/java/com/kkkoke/Main.java
@@ -0,0 +1,12 @@
+package com.kkkoke;
+
+/**
+ * @author KeyCheung
+ * @date ${YEAR}/${MONTH}/${DAY}
+ * @desc
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
